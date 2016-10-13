b84680bbe1905410ec718d86bc19a0df7df58c1b
mathieuchot <mathieuchot@users.noreply.github.com>
2016-10-13 15:30:51 +0200

    Update mail-generator.py

--- mail-generator.py
+++ mail-generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-#mail generator
-#blog.mathieu.chot-plassot.com
+# coding: utf8
+#Mail generator
 
 import sys
 import requests
@@ -8,18 +8,42 @@ import time
 import json
 import argparse
 
-parser = argparse.ArgumentParser(description="A simple mail checker",\
-        epilog="Exemple: {} -f 'mathieu' -l 'chot-plassot' -s '1337' -p 'protonmail.com'".format(sys.argv[0]))
-parser.add_argument("-f", "--firstname",action="store", type=str, required=True,\
-        help="Firstname of the victim")
-parser.add_argument("-l", "--lastname",action="store", type=str, required=True,\
-        help="Lastname of the victim")
+ascii_art='''\033[0;36m          
+        .wwwwwwww.         
+(⌒▽⌒) .w"  "WW"  "w.     
+     ."   ~\  /~   ".   
+    |\     O  O     /|  
+    \|, ()__\/__() ,|/  ~(‾▿‾)~
+     / \ \_v__v_/ / \   
+    / | \________/ | \\
+    >  \ mathieu  /  <
+,`  \   \   ""   /   /  
+     \,  \ chot /  ,/   ٩(͡๏̯͡๏)۶
+----,________________,-------`'-
+    |   EVIL PWNER   |  
+    |   28/09/2016   |
+    `----------------'  \033[0;0m
+'''
+print ascii_art
+
+usage_help="\033[0;32mExemple with -n:\033[0;36m {} -n 'mathieu' \
+        'chot-plassot' [-s '1337'] [-p 'protonmail.com']\
+            \n\033[0;32mExemple with -u: \033[0;36m {} -u 'gentilkiwi'\
+            [-p 'me.com']\033[0;0m".format(sys.argv[0],sys.argv[0])
+
+parser = argparse.ArgumentParser(description="A simple mail checker")
+parser.add_argument("-n", "--name", action="store", type=str, required=False,\
+        help="[0]First name and [1]Lastname of the victim", nargs=2)
+parser.add_argument("-u", "--username",action="store", default='', type=str, required=False,\
+        help="Username of the victim")
 parser.add_argument("-s", "--spec", action="store",type=str, default='', required=False,\
         help="Age, Birthdate, zipcode, something to add...")
 parser.add_argument("-p", "--provider",action="store", type=str, required=False,\
         help="A mail provider to add")
 params = parser.parse_args()
-
+if len(sys.argv) < 2:
+    print usage_help
+    sys.exit()
 
 headers = {'User-agent': 'mathieuchot haveibeenpwn script'}
 providers=['hotmail.fr', 'free.fr', 'orange.fr', 'live.fr',\
@@ -28,37 +52,46 @@ providers=['hotmail.fr', 'free.fr', 'orange.fr', 'live.fr',\
 
 if params.provider:
     providers.append(params.provider)
-firstname=params.firstname
-lastname=params.lastname
 def gen_mail(spec):
     mails=[]
-    for i in providers:
-        mails.append("{}{}{}@{}".format(lastname,firstname,spec,i))
-        mails.append("{}{}{}@{}".format(firstname,lastname,spec,i))
-        mails.append("{}{}{}@{}".format(lastname[:1],firstname,spec,i))
-        mails.append("{}{}{}@{}".format(firstname,lastname[:1],spec,i))
-        mails.append("{}{}{}@{}".format(firstname[:1],lastname,spec,i))
-        mails.append("{}.{}{}@{}".format(lastname,firstname,spec,i))
-        mails.append("{}_{}{}@{}".format(lastname,firstname,spec,i))
-        mails.append("{}-{}{}@{}".format(lastname,firstname,spec,i))
-        mails.append("{}.{}{}@{}".format(firstname,lastname,spec,i))
-        mails.append("{}_{}{}@{}".format(firstname,lastname,spec,i))
-        mails.append("{}-{}{}@{}".format(firstname,lastname,spec,i))
-        mails.append("{}{}{}@{}".format(firstname,lastname[:3],spec,i))
-        mails.append("{}{}{}@{}".format(firstname,lastname[:4],spec,i))
-        mails.append("{}.{}{}@{}".format(firstname,lastname[:3],spec,i))
-        mails.append("{}.{}{}@{}".format(firstname,lastname[:4],spec,i))
-        mails.append("{}_{}{}@{}".format(firstname,lastname[:3],spec,i))
-        mails.append("{}_{}{}@{}".format(firstname,lastname[:4],spec,i))
-        mails.append("{}-{}{}@{}".format(firstname,lastname[:3],spec,i))
-        mails.append("{}-{}{}@{}".format(firstname,lastname[:4],spec,i))
-        mails.append("{}{}{}@{}".format(lastname[:3],firstname,spec,i))
-        mails.append("{}{}{}@{}".format(lastname[:4],firstname,spec,i))
-        mails.append("{}.{}{}@{}".format(lastname[:4],firstname,spec,i))
-        mails.append("{}.{}{}@{}".format(lastname[:3],firstname,spec,i))
-    return mails
-
+    if params.name:
+        firstname=params.name[0]
+        lastname=params.name[1]
+        for i in providers:
+            mails.append("{}{}{}@{}".format(lastname,firstname,spec,i))
+            mails.append("{}{}{}@{}".format(firstname,lastname,spec,i))
+            mails.append("{}{}{}@{}".format(lastname[:1],firstname,spec,i))
+            mails.append("{}{}{}@{}".format(firstname,lastname[:1],spec,i))
+            mails.append("{}{}{}@{}".format(firstname[:1],lastname,spec,i))
+            mails.append("{}.{}{}@{}".format(lastname,firstname,spec,i))
+            mails.append("{}_{}{}@{}".format(lastname,firstname,spec,i))
+            mails.append("{}-{}{}@{}".format(lastname,firstname,spec,i))
+            mails.append("{}.{}{}@{}".format(firstname,lastname,spec,i))
+            mails.append("{}_{}{}@{}".format(firstname,lastname,spec,i))
+            mails.append("{}-{}{}@{}".format(firstname,lastname,spec,i))
+            mails.append("{}{}{}@{}".format(firstname,lastname[:3],spec,i))
+            mails.append("{}{}{}@{}".format(firstname,lastname[:4],spec,i))
+            mails.append("{}.{}{}@{}".format(firstname,lastname[:3],spec,i))
+            mails.append("{}.{}{}@{}".format(firstname,lastname[:4],spec,i))
+            mails.append("{}_{}{}@{}".format(firstname,lastname[:3],spec,i))
+            mails.append("{}_{}{}@{}".format(firstname,lastname[:4],spec,i))
+            mails.append("{}-{}{}@{}".format(firstname,lastname[:3],spec,i))
+            mails.append("{}-{}{}@{}".format(firstname,lastname[:4],spec,i))
+            mails.append("{}{}{}@{}".format(lastname[:3],firstname,spec,i))
+            mails.append("{}{}{}@{}".format(lastname[:4],firstname,spec,i))
+            mails.append("{}.{}{}@{}".format(lastname[:4],firstname,spec,i))
+            mails.append("{}.{}{}@{}".format(lastname[:3],firstname,spec,i))
+        return mails
+    elif params.username:
+        for i in providers:
+            mails.append("{}@{}".format(params.username, i))
+        return mails
+    else:
+        print usage_help
+        sys.exit()
 if params.spec: 
+    if params.username:
+        print "\033[0;33m Skipping... -s is not compatible with -u\033[0;0m"
     result=gen_mail(params.spec)
     result2=gen_mail('')
     for i in result2:
